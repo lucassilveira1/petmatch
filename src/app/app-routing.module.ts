a1943d9a601b37abce79973701108e1bb64371b7
Lucas Silveira <lucas.silveira0516@gmail.com>
2024-05-24 19:08:49 -0300

    feat/frontend: css adjusts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,43 +4,45 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   {
     path: 'home',
-    loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
+    loadChildren: () =>
+      import('./home/home.module').then((m) => m.HomePageModule),
   },
   {
     path: '',
     redirectTo: 'home',
-    pathMatch: 'full'
-  },
-  {
-    path: 'login',
-    loadChildren: () => import('./pages/login/login.module').then( m => m.LoginPageModule)
+    pathMatch: 'full',
   },
   {
     path: 'signup',
-    loadChildren: () => import('./pages/signup/signup.module').then( m => m.SignupPageModule)
+    loadChildren: () =>
+      import('./pages/signup/signup.module').then((m) => m.SignupPageModule),
   },
   {
     path: 'petinfo',
-    loadChildren: () => import('./pages/petinfo/petinfo.module').then( m => m.PetinfoPageModule)
+    loadChildren: () =>
+      import('./pages/petinfo/petinfo.module').then((m) => m.PetinfoPageModule),
   },
   {
     path: 'ongsign',
-    loadChildren: () => import('./pages/ongsign/ongsign.module').then( m => m.OngsignPageModule)
-  },  {
+    loadChildren: () =>
+      import('./pages/ongsign/ongsign.module').then((m) => m.OngsignPageModule),
+  },
+  {
     path: 'feed',
-    loadChildren: () => import('./pages/feed/feed.module').then( m => m.FeedPageModule)
+    loadChildren: () =>
+      import('./pages/feed/feed.module').then((m) => m.FeedPageModule),
   },
   {
     path: 'profile',
-    loadChildren: () => import('./pages/profile/profile.module').then( m => m.ProfilePageModule)
+    loadChildren: () =>
+      import('./pages/profile/profile.module').then((m) => m.ProfilePageModule),
   },
-
 ];
 
 @NgModule({
   imports: [
-    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })
+    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules }),
   ],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
